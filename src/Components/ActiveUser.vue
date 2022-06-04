4766b3c699e864a56c4c251fe0d2b1f6ca479294
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 23:12:25 +0300

    Exercise on component communication

---
+++ src/Components/ActiveUser.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <header>
+            <h1>{{ username}}</h1>
+            <h3>{{ userage}} Years</h3>
+        </header>
+    </section>
+</template>
+
+<script>
+
+export default {
+
+props : {
+    
+    username : {
+        type : String,
+        required : true
+    },
+    userage : {
+        type : Number,
+        required : true
+    },
+}
+
+  
+}
+
+</script>
+
+
+
---
+++ src/Components/UserData.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <div>
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Your name" v-model="enteredName" />
+            </div>
+            <div>
+                <label for="age">Age</label>
+                <input type="number" name="age" id="age" placeholder="Your age" v-model="enteredAge" />
+            </div>
+
+            <div>
+                <button>Set User Data</button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+
+    emits : ['set-data'],
+
+   data (){
+       return {
+           enteredName : '',
+           enteredAge : '',
+       }
+   },
+
+   methods : {
+       submitData(){
+
+           this.$emit('set-data',this.enteredName,this.enteredAge)
+           this.enteredName =''
+           this.enteredAge =''
+       }
+   }
+}
+
+</script>
